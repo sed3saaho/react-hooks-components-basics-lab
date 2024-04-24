67fd3b58d68b6ec5fb586e06b02ddf1cba1aaba1
Sedly <sedly.keith@student.moringaschool.com>
2024-04-24 12:06:13 +0300

    react-hooks-components-basics-lab

--- src/components/App.js
+++ src/components/App.js
@@ -16,20 +16,10 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */
 function About() {
   return (
     <div id="about">
-      <h2>About Us</h2>
-      <p>Welcome to our website! Learn more about us here.</p>
-    </div>
-  );
-}}
-
-function About() {
-  return (
-    <div id="about">
-      <h2>About</h2>
+      <h1>About</h1>
     </div>
   );
 }
@@ -39,11 +29,9 @@ function About() {
 function App() {
   return (
     <div>
-     
       <NavBar />
       <Home />
       <About />
-      
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
     </div>
